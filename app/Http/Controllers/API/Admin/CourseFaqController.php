fea6b7c5404200e45daf5e6040cc5cab696a92fb
SwarnaliSaha-Dev7 <dev7.scwt@gmail.com>
2025-04-10 17:27:20 +0530

    Course Review, homepage api fix, faq in progress

--- app/Http/Controllers/API/Admin/CourseFaqController.php
+++ app/Http/Controllers/API/Admin/CourseFaqController.php
@@ -8,6 +8,7 @@ use App\Models\CourseFaq;
 use App\Http\Controllers\API\Exception;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\DB;
 use Validator;
 
 class CourseFaqController extends Controller
@@ -18,21 +19,38 @@ class CourseFaqController extends Controller
 
             $validator = Validator::make($request->all(), [
                 'course_id' => 'required',
-                'question' => 'required',
-                'answer' => 'required',
+                'faqs' => 'required',
+                // 'question' => 'required',
+                // 'answer' => 'required',
             ]);
 
             if ($validator->fails()) {
                 return sendErrorResponse('Validation Error.', $validator->errors(), 400);
             }
 
-            $storeInfo = CourseFaq::create([
-                            'course_id' => $request->course_id,
-                            'question' => $request->question,
-                            'answer' => $request->answer,
-                        ]);
+            // $storeInfo = CourseFaq::create([
+            //                 'course_id' => $request->course_id,
+            //                 'question' => $request->question,
+            //                 'answer' => $request->answer,
+            //             ]);
 
-            return sendSuccessResponse('FAQ inserted successfully.', '');
+            //store faqs
+            $faqs = json_decode($request->faqs);
+            $now = \Carbon\Carbon::now();
+            $faqs = array_map(function ($faq) use ($now, $request){
+                return [
+                    'course_id' => $request->course_id,
+                    'question' => $faq->question,
+                    'answer' => $faq->answer,
+                    'created_at' => $now,
+                    'updated_at' => $now,
+                ];
+
+            }, $faqs);
+
+            CourseFaq::insert($faqs);
+
+            return sendSuccessResponse('FAQs inserted successfully.', '');
         } catch (\Throwable $th) {
             return sendErrorResponse('Something went wrong.', $th->getMessage(), 500);
         }
@@ -43,8 +61,13 @@ class CourseFaqController extends Controller
         try {
 
             $data = CourseFaq::with('course:id,course_name')
-                                ->select('id','course_id','question','answer','created_at','updated_at')
-                                ->orderBy('id','desc')
+                                // ->select('id','course_id','question','answer','created_at','updated_at')
+                                ->select(
+                                    'course_id',
+                                    DB::raw('GROUP_CONCAT(CONCAT("Q:", question, ", A:", answer)  SEPARATOR " || ") as faqs')
+                                    )
+                                ->groupBy('course_id')
+                                // ->orderBy('id','desc')
                                 ->paginate(15);
 
             return sendSuccessResponse('All FAQs fetched successfully.', $data);
--- app/Http/Controllers/API/Admin/CourseReviewController.php
+++ app/Http/Controllers/API/Admin/CourseReviewController.php
@@ -9,6 +9,7 @@ use App\Models\CourseReview;
 use App\Http\Controllers\API\Exception;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\DB;
 use Validator;
 
 class CourseReviewController extends Controller
@@ -26,12 +27,27 @@ class CourseReviewController extends Controller
                 return sendErrorResponse('Validation Error.', $validator->errors(), 400);
             }
 
-            $storeInfo = CourseReview::create([
-                            'course_id' => $request->course_id,
-                            'review' => $request->review,
-                        ]);
+            // $storeInfo = CourseReview::create([
+            //                 'course_id' => $request->course_id,
+            //                 'review' => $request->review,
+            //             ]);
 
-            return sendSuccessResponse('Review inserted successfully.', '');
+            //store reviews
+            $reviews = json_decode($request->review);
+            $now = \Carbon\Carbon::now();
+            $reviews = array_map(function ($text) use ($now, $request){
+                return [
+                    'course_id' => $request->course_id,
+                    'review' => $text,
+                    'created_at' => $now,
+                    'updated_at' => $now,
+                ];
+
+            }, $reviews);
+
+            CourseReview::insert($reviews);
+
+            return sendSuccessResponse('Reviews inserted successfully.', '');
         } catch (\Throwable $th) {
             // Log::error('Review inserted error: ' . $th->getMessage());
             return sendErrorResponse('Something went wrong.', $th->getMessage(), 500);
@@ -43,35 +59,46 @@ class CourseReviewController extends Controller
         try {
 
             $data = CourseReview::with('course:id,course_name')
-                                ->select('id','course_id','review','created_at','updated_at')
-                                ->orderBy('id','desc')
+                                // ->select('id','course_id','review','created_at','updated_at')
+                                ->select(
+                                    'course_id',
+                                    DB::raw('GROUP_CONCAT(review SEPARATOR " || ") as reviews'))
+                                // ->orderBy('id','desc')
+                                ->groupBy('course_id')
                                 ->paginate(15);
 
             return sendSuccessResponse('All reviews fetched successfully.', $data);
         } catch (\Throwable $th) {
-            // Log::error('All reviews fetched error: ' . $th->getMessage());
             return sendErrorResponse('Something went wrong.', $th->getMessage(), 500);
         }
     }
 
-    public function updatedDataFetch(Request $request, $id): JsonResponse
+    public function updatedDataFetch(Request $request, $course_id): JsonResponse
     {
         try {
 
-            $data = CourseReview::select('id','course_id','review','created_at','updated_at')
-                                ->find($id);
+            // $data = CourseReview::select('id','course_id','review','created_at','updated_at')
+            //                     ->find($id);
 
-            if (!$data) {
+            $reviews = CourseReview::with('course:id,course_name')
+                                ->where('course_id', $course_id)
+                                ->get();
+
+            if (!count($reviews)) {
                 return sendErrorResponse('Data not found.', '', 404);
             }
+
+            $data['course_id'] = $reviews->first()->course_id;
+            $data['course_name'] = $reviews->first()->course->course_name;
+            $data['reviews'] = $reviews->pluck('review');
+
             return sendSuccessResponse('Review data fetched successfully.', $data);
         } catch (\Throwable $th) {
-            // Log::error('Review data fetched error: ' . $th->getMessage());
             return sendErrorResponse('Something went wrong.', $th->getMessage(), 500);
         }
     }
 
-    public function update(Request $request, $id): JsonResponse
+    public function update(Request $request, $course_id): JsonResponse
     {
         try {
 
@@ -83,35 +110,53 @@ class CourseReviewController extends Controller
                 return sendErrorResponse('Validation Error.', $validator->errors(), 400);
             }
 
-            $checkData = CourseReview::find($id);
+            $checkData = CourseReview::where('course_id', $course_id)->get();
 
-            if (!$checkData) {
+            if (!count($checkData)) {
                 return sendErrorResponse('Data not found.', '', 404);
             }
 
-            $storeInfo = CourseReview::where('id', $id)
-                                        ->update(['review' => $request->review,]);
+            //delete prev reviews
+            CourseReview::where('course_id', $course_id)->delete();
 
-            return sendSuccessResponse('Review updated successfully.', '');
+            //store reviews
+            $reviews = json_decode($request->review);
+            $now = \Carbon\Carbon::now();
+            $reviews = array_map(function ($text) use ($now, $course_id){
+                return [
+                    'course_id' => $course_id,
+                    'review' => $text,
+                    'created_at' => $now,
+                    'updated_at' => $now,
+                ];
+
+            }, $reviews);
+
+            CourseReview::insert($reviews);
+
+            // $storeInfo = CourseReview::where('id', $id)
+            //                             ->update(['review' => $request->review,]);
+
+            return sendSuccessResponse('Reviews updated successfully.', '');
         } catch (\Throwable $th) {
             // Log::error('Review updated fetched error: ' . $th->getMessage());
             return sendErrorResponse('Something went wrong.', $th->getMessage(), 500);
         }
     }
 
-    public function destroy(Request $request, $id): JsonResponse
+    public function destroy(Request $request, $course_id): JsonResponse
     {
         try {
 
-            $checkData = CourseReview::find($id);
+            $checkData = CourseReview::where('course_id', $course_id)->get();
 
-            if (!$checkData) {
+            if (!count($checkData)) {
                 return sendErrorResponse('Data not found.', '', 404);
             }
 
-            $checkData->delete();
+            CourseReview::where('course_id', $course_id)->delete();
 
-            return sendSuccessResponse('Review deleted successfully.', '');
+            return sendSuccessResponse('Reviews deleted successfully.', '');
         } catch (\Throwable $th) {
             // Log::error('Review deleted error: ' . $th->getMessage());
             return sendErrorResponse('Something went wrong.', $th->getMessage(), 500);
